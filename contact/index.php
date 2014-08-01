a6762836a2e1e1e23b4d685c1f6d4144fdd0e67e
Steven Palmer <steven@stevenrpalmer.com>
2014-08-02 01:58:28 +0800

    Revert "Revert "Added contact page""
    This reverts commit 14627714df075cce60a8fa5fabc64feb82a1ecc2.

---
+++ contact/index.php
@@ -0,0 +1,155 @@
+<?php $thisPage="developer"; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Volt - Spark your Business Potential">
+    <meta name="author" content="Techworks">
+    <link rel="shortcut icon" href="/images/favicon.ico">
+
+    <title>Volt - <?php if ($thisPage=="about") echo "Spark your Business Potential"; ?>
+                  <?php if ($thisPage=="landing") echo "Spark your Business Potential"; ?>
+    </title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="../css/style.css">
+
+    <!-- Superslides -->
+    <link rel="stylesheet" href="../css/superslides.css">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <script>
+  // GOOGLE ANALYTICS CODE
+  </script>
+
+  <body>
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container">
+
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/volt">Brand</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-hover="dropdown">Overview</a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="/volt/overview/introduction">Introduction</a></li>    
+            <li><a href="/volt/overview/developer">Developer</a></li>       
+            <li><a href="/volt/overview/investment">Contact</a></li>       
+            <li><a href="#">Other Developments</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-hover="dropdown">Why Iskandar Malaysia</a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="/volt/why-iskandar-malaysia/location">Location</a></li>       
+            <li><a href="/volt/why-iskandar-malaysia/investment">Investment</a></li>       
+            <li><a href="/volt/why-iskandar-malaysia/incentives">Incentives</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-hover="dropdown">Retail Offices</a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Overview</a></li>       
+            <li><a href="#">Facilities</a></li>       
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Registeration</a></li>            
+          </ul>
+        </li>   
+        <li>
+          <a href="/volt/contact">Contact</a>
+        </li>                        
+      </ul>
+    </div><!-- /.navbar-collapse -->
+
+  </div><!-- /.container-fluid -->
+
+</nav>
+
+<div class="jumbotron main">
+      <img src="../images/overview.jpg" class="img-responsive" alt="location" >
+</div>
+
+<div class="darkblue-bg">
+	<div class="container"> 
+
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3">
+				<h2>
+					This is the Contact Page
+				</h2>
+			</div>
+		</div>
+
+		<div class="row">	
+			<div class="col-sm-6 col-sm-offset-3">
+				<p class="center">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+ 				</p>	
+			</div>
+		</div>
+
+	</div>		
+</div>
+
+    <div id="footer">
+      <div class="container">
+        <a href="/volt/">
+          <div class="footer-logo">Volt footer</div>
+        </a>
+
+          <ul class="footer-links">
+            <li>Overview</li>            
+            <li>Why Iskandar Malaysia</li>
+            <li>Retail Offices</li>                        
+          </ul>
+<!--           <ul class="footer-privacy">
+            <li>© Copyright</li>            
+            <li>Volt Corporate Park @ Medini 2014 </li>
+            <li>Disclaimer</li>                        
+          </ul> -->
+
+
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/bootstrap-hover-dropdown.js"></script>    
+    <script src="../js/jquery.easing.1.3.js"></script>
+    <script src="../js/jquery.animate-enhanced.min.js"></script>
+
+  </body>
+</html>
+
+
+
+
